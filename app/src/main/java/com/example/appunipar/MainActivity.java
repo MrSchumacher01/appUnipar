026fa7f3945ad33d79b85db84519cdab3513db3c
Marcos Schumacher <marcosrschumacher@gmail.com>
2024-10-06 18:33:42 -0300

    Adicionada funcionalidade básica de clique no botão

--- app/src/main/java/com/example/appunipar/MainActivity.java
+++ app/src/main/java/com/example/appunipar/MainActivity.java
@@ -1,12 +1,28 @@
 package com.example.myapplication;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Referenciando o botão no layout
+        Button button = findViewById(R.id.my_button);
+
+        // Definindo um listener para o clique no botão
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Exibir uma mensagem ao clicar no botão
+                Toast.makeText(MainActivity.this, "Botão clicado!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
