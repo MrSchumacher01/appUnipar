7d7c75d83b3c44486b11253c17a7176dff3c71a3
Marcos Schumacher <marcosrschumacher@gmail.com>
2024-10-06 21:19:16 -0300

    Adiciona integração inicial de tabs e viewpager com fragments de previsão e sobre

--- app/src/main/java/com/example/appunipar/AboutFragment.java
+++ app/src/main/java/com/example/appunipar/AboutFragment.java
@@ -1,16 +1,19 @@
 package com.example.appunipar;
 
 import android.os.Bundle;
-import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
 
 public class AboutFragment extends Fragment {
 
+    @Nullable
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        // Inflando o layout para este fragment
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 }
