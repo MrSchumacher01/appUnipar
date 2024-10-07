6c7c32b3d4a304089a5884a17d2faeb9408db11d
Marcos Schumacher <marcosrschumacher@gmail.com>
2024-10-07 16:03:26 -0300

    Adicionado ícone de informações e corrigido menu da AppBar

---
+++ app/src/main/java/com/example/appunipar/AboutActivity.java
@@ -0,0 +1,13 @@
+package com.example.appunipar;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class AboutActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.fragment_about); // Aqui utiliza o layout que já temos
+    }
+}
--- app/src/main/java/com/example/appunipar/MainActivity.java
+++ app/src/main/java/com/example/appunipar/MainActivity.java
@@ -1,8 +1,13 @@
 package com.example.appunipar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.viewpager.widget.ViewPager;
+
 import com.google.android.material.tabs.TabLayout;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,5 +23,25 @@ public class MainActivity extends AppCompatActivity {
         TabPagerAdapter adapter = new TabPagerAdapter(getSupportFragmentManager());
         viewPager.setAdapter(adapter);
         tabLayout.setupWithViewPager(viewPager);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_about) {
+            // Navega para a tela "Sobre"
+            Intent intent = new Intent(MainActivity.this, AboutActivity.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
